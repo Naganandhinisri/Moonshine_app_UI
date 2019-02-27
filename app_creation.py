4cd4fe27916653e2c90c4c0c5ecbe6da027415cb
Techops <techops@Intern4-MacBook-Pro.local>
2019-02-27 17:09:47 +0530

    Display Flow of ordering Cold Beverages

--- app_creation.py
+++ app_creation.py
@@ -12,11 +12,6 @@ def homes():
     return render_template('login_page.html')
 
 
-@app.route('/ordering_page')
-def page2():
-    return render_template('ordering_page.html')
-
-
 def validate_data(connection, user_data):
     cursor = connection.cursor()
     cursor.execute("select employee_id from staff_details where employee_id=%(id)s",
@@ -34,22 +29,19 @@ def data():
     return validate_data(connection, request.form)
 
 
-@app.route('/items', methods=['GET'])
-def hello_():
-    table = database_connect()
-    return render_template("items_display.html", items=table)
-
-
 @app.route('/availability', methods=['POST'])
 def hello():
     table = database_connect()
-    return render_template("items_display.html", items=table)
+    items = []
+    for row in table:
+        items.append(row[0])
+    return render_template("items_display.html", items=items)
 
 
 def database_connect():
     try:
         cursor = connection.cursor()
-        cursor.execute("select  Available_juices from list_juices" )
+        cursor.execute("select  Available_juices from list_juices")
         record = cursor.fetchall()
 
         return record
@@ -57,35 +49,17 @@ def database_connect():
         print("Error while connecting to PostgreSQL", error)
 
 
-@app.route('/post-data', methods=['POST', 'GET'])
-def get_data():
-    post_data(connection, request.form)
-    return render_template('employee_flow.html', shared=request.form)
+@app.route('/selected_beverages', methods=['POST'])
+def beverages():
+    selected_beverages(connection, request.form)
+    return render_template('flow.html', shared=request.form)
 
 
-def post_data(connection, user_data):
+def selected_beverages(connection, user_data):
     cursor = connection.cursor()
-    cursor.execute(" insert into cold_beverages(employee_id) values(" + user_data['numeric'] + ") ")
+    cursor.execute(" insert into cold_beverages(employee_id) values("+user_data['numeric']+") ")
     connection.commit()
     cursor.close()
-
-
-@app.route('/employee_flow', methods=['POST'])
-def hello():
-    row = table_connect()
-    return render_template("employee_flow.html", items=row)
-
-
-def table_connect():
-    try:
-        cursor = connection.cursor()
-        cursor.execute("select  employee_id from cold_beverages" )
-        records = cursor.fetchall()
-
-        return records
-    except (Exception, psycopg2.Error) as error:
-        print("Error while connecting to PostgreSQL", error)
-
 
 
 if __name__ == '__main__':
